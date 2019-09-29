ecf4dbbfd8b6295c09d968c5bd7f64a35bb74102
Pekka Salmela <pekka.salmela@gmail.com>
2019-09-29 18:47:16 +0300

    Render Markdown for ancient enemy

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,70 +1,74 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { Badge } from 'react-bootstrap'
-import ReactMarkdown from 'react-markdown'
-import Rune from './Rune'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Badge } from 'react-bootstrap';
+import ReactMarkdown from 'react-markdown';
+import Rune from './Rune';
 
 class Results extends Component {
   render() {
-    const results = this.props.results
+    const results = this.props.results;
     return (
-      results &&
-      <div className="results">
-        <h4>Runes</h4>
-        <div className="runes-results">
-          {
-            results.rune_modifiers &&
-            Object.entries(results.rune_modifiers).map(([k, v]) =>
-              <p key={`rune-modifier-${k}`}>
-                <span>{k} <Rune rune={k} /> <Badge>{v}</Badge></span>
-              </p>
-            )
-          }
-        </div>
-        <h4>Resources</h4>
-        <div className="resource-results">
-          {
-            results.resource_modifiers &&
-            Object.entries(results.resource_modifiers).map(([k, v]) =>
-              <p key={`resource-modifier-${k}`}>
-                <span>{k} <Badge>{v}</Badge></span>
-              </p>
-            )
-          }
-        </div>
-        <h4>Virtues</h4>
-        <div className="virtue-results">
-          {
-            results.virtue_modifiers &&
-            Object.entries(results.virtue_modifiers).map(([k, v]) =>
-              <p key={`virtue-modifier-${k}`}>
-                <span>{k} <Badge>{v}</Badge></span>
-              </p>
-            )
-          }
-        </div>
+      results && (
+        <div className="results">
+          <h4>Runes</h4>
+          <div className="runes-results">
+            {results.rune_modifiers &&
+              Object.entries(results.rune_modifiers).map(([k, v]) => (
+                <p key={`rune-modifier-${k}`}>
+                  <span>
+                    {k} <Rune rune={k} /> <Badge>{v}</Badge>
+                  </span>
+                </p>
+              ))}
+          </div>
+          <h4>Resources</h4>
+          <div className="resource-results">
+            {results.resource_modifiers &&
+              Object.entries(results.resource_modifiers).map(([k, v]) => (
+                <p key={`resource-modifier-${k}`}>
+                  <span>
+                    {k} <Badge>{v}</Badge>
+                  </span>
+                </p>
+              ))}
+          </div>
+          <h4>Virtues</h4>
+          <div className="virtue-results">
+            {results.virtue_modifiers &&
+              Object.entries(results.virtue_modifiers).map(([k, v]) => (
+                <p key={`virtue-modifier-${k}`}>
+                  <span>
+                    {k} <Badge>{v}</Badge>
+                  </span>
+                </p>
+              ))}
+          </div>
 
-        <h4>Ancient enemy</h4>
-        <p>{results.ancient_enemy || "None"}</p>
-        <h4>New enemy</h4>
-        <p>{results.new_enemy || "None"}</p>
-        <h4>Wyter abilities</h4>
-        {
-          results.wyter_abilities &&
-          results.wyter_abilities.map((ability, i) =>
-            <p key={`ability-${i}`}>{ability}</p>
-          )
-        }
-        <h4>Special effects</h4>
-        <div className="specials-results">
-          {
-            results.specials &&
-            results.specials.map((s, i) =>
-              <ReactMarkdown key={`special-${i}`}>{s}</ReactMarkdown>)
-          }
+          <h4>Ancient enemy</h4>
+          {results.ancient_enemy ? (
+            <ReactMarkdown>{results.ancient_enemy}</ReactMarkdown>
+          ) : (
+            <p>None</p>
+          )}
+
+          <h4>New enemy</h4>
+          <p>{results.new_enemy || 'None'}</p>
+
+          <h4>Wyter abilities</h4>
+          {results.wyter_abilities &&
+            results.wyter_abilities.map((ability, i) => (
+              <p key={`ability-${i}`}>{ability}</p>
+            ))}
+          <h4>Special effects</h4>
+          <div className="specials-results">
+            {results.specials &&
+              results.specials.map((s, i) => (
+                <ReactMarkdown key={`special-${i}`}>{s}</ReactMarkdown>
+              ))}
+          </div>
         </div>
-      </div>
-    )
+      )
+    );
   }
 }
 
@@ -78,6 +82,6 @@ Results.propTypes = {
     new_enemy: PropTypes.string,
     wyter_abilities: PropTypes.arrayOf(PropTypes.string)
   })
-}
+};
 
-export default Results
+export default Results;
